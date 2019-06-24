d04e58905f91e5e69f7b9446a39f4721abe4566e
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-06-24 20:25:16 +0300

    displaying products from api

--- js/shop.js
+++ js/shop.js
@@ -8,7 +8,35 @@ window.Shop = {
             method: "GET"
         }).done(function (response) {
             console.log(response);
+            Shop.displayProducts(response.content)
         });
+    },
+
+    getProductHtml: function (product) {
+        return `<div class="col-md-3 col-sm-6">
+                    <div class="single-shop-product">
+                        <div class="product-upper">
+                            <img src="img/product-2.jpg" alt="">
+                        </div>
+                        <h2><a href="">${product.name}</a></h2>
+                        <div class="product-carousel-price">
+                            <ins>$${product.price}</ins>
+                        </div>  
+                        
+                        <div class="product-option-shop">
+                            <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="${product.id}" rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add to cart</a>
+                        </div>                       
+                    </div>
+                </div>`
+    },
+
+    displayProducts: function (products) {
+        let productsHtml = "";
+
+        products.forEach(product => productsHtml += Shop.getProductHtml(product));
+
+        // cssSelector
+        $('#products-container').html(productsHtml);
     }
 };
 
